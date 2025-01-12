e3119420ba42fe417275d2488cc58d4184a8c78a
Rabih21 <86662088+Rabih21@users.noreply.github.com>
2025-01-12 22:49:13 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Optional JavaScript for smooth scrolling (if desired)
+document.querySelectorAll('.navbar a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
